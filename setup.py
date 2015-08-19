51337c5fa3fe21ccfadbc26f19aa9f2574663fdc
Wilberto Morales <wilbertomorales777@gmail.com>
2015-08-18 19:17:41 -0500

    Update version after Apache configuration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='flask-reverse-proxy',
-    version='0.1.0.0',
+    version='0.2.0.0',
     packages=['flask_reverse_proxy'],
     url='',
     license='',
