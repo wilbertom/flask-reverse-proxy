24476f5a83e7183a2624b556b1b37c8c61591549
Low Kian Seong <lowks@users.noreply.github.com>
2016-03-24 23:38:38 +0800

    Update setup.py
    Adding `description` to `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='flask-reverse-proxy',
     version='0.2.0.0',
+    description='A Flask extension for applications in a reverse proxy not at the root',
     packages=['flask_reverse_proxy'],
     url='',
     license='',
