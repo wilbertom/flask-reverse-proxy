d0d0bdd27ec89b7e5e4bec51ae7b53232841acf1
Wilberto Morales <wilbertomorales777@gmail.com>
2016-03-28 21:23:08 -0500

    Update version to 2.0.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup(
     name='flask-reverse-proxy',
-    version='0.2.0.1',
+    version='0.2.0.2',
     description='A Flask extension for applications in a reverse proxy not at the root',
     packages=['flask_reverse_proxy'],
     url='',
     license='',
     author='Wilberto Morales',
     author_email='wilbertomorales777@gmail.com',
-    description=''
 )
